8213a7222acc9dd882e0f52c1f05a539526c4df5
kjh03160 <kis03160@likelion.org>
2020-08-29 00:05:27 +0900

    nowwaiting

--- now_waiting.py
+++ now_waiting.py
@@ -1,5 +1,8 @@
 from selenium_class import Driver
 import time
+import json
+
+
 def now_waiting(driver, date):
     url = "https://nowwaiting.co/api/v2/spots/5185/stats/orders?created_on={date}&page=1&count=1000&order_type=&payment_type=".format(date = date)
     login_url = "https://ceo.nowwaiting.co/order_sales/history"
@@ -7,20 +10,24 @@ def now_waiting(driver, date):
 
     driver.get_url(url)
     pre = driver.find_by_tag("pre").text
-    with open('data/dummy.json', 'w', encoding='UTF-8')as file:
-        file.write(pre)
+    j = json.loads(pre)['data']['orders']
+    with open('data/now_waiting/' + date + '.json', 'w', encoding='UTF-8')as file:
+        file.write(json.dumps(j))
 
 
-    import json
-    import pandas as pd
-    with open('data/dummy.json', 'r', encoding='UTF-8')as file:
-        x = json.load(file)['data']['orders']
-        if len(x) == 0:
-            print("주문 내역이 없습니다.")
-            return driver
-
-        df = pd.DataFrame(x)
-        df.to_csv('data/now_waiting/' + date + '.csv', index=False, mode='w', encoding='utf-8-sig')
+    # import pandas as pd
+    # with open('data/dummy.json', 'r', encoding='UTF-8')as file:
+    #     x = json.load(file)['data']['orders']
+    #     if len(x) == 0:
+    #         print("주문 내역이 없습니다.")
+    #         return driver
+    #
+    #     with open('data/now_waiting/' + date + '.json', 'w', encoding='UTF-8') as file2:
+    #         file2.write(x)
+    #
+    #
+    #     df = pd.DataFrame(x)
+    #     df.to_csv('data/now_waiting/' + date + '.csv', index=False, mode='w', encoding='utf-8-sig')
 
     return driver
 
