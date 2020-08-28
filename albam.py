662fbb93d37d23270f8103d94089da66076c85c0
kjh03160 <kis03160@likelion.org>
2020-08-29 00:05:43 +0900

    albam

--- albam.py
+++ albam.py
@@ -1,19 +1,18 @@
 from selenium_class import Driver
 import datetime
 import pandas as pd
-
+import  time
 def albam(driver, date):
 
     url = "https://web.albamapp.com/today"
     driver.get_url(url)
 
     navs = driver.find_by_css("ul.nav")
-    lis = driver.find_by_link_with_obj(navs, "근무기록")
+    lis = driver.find_by_link_with_obj(navs, "급여")
     driver.click(lis)
 
-
+    time.sleep(0.5)
     date_pickers = driver.find_all_by_css("div.react-datepicker__input-container > button")
-
     start_date = list(map(int, (date_pickers[0].text.split()[0].split("-"))))
     start_date = datetime.datetime(start_date[0], start_date[1], start_date[2])
 
@@ -28,48 +27,50 @@ def albam(driver, date):
             driver.click(date_pickers[i])
             month = int(driver.find_by_css("div.react-datepicker__current-month").text.split()[0][:-1])
             year = int(driver.find_by_css("div.react-datepicker__current-month").text.split()[1])
-
-            if now_date[1] < month:
+            dff = (year - now_date[0]) * 12 + (month - now_date[1])
+            if dff:
                 prev = driver.find_by_css("button.react-datepicker__navigation--previous")
-                count = month - now_date[1]
-                if now_date[0] < year:
-                    count += 12
-                for i in range(count):
+
+                for i in range(dff):
                     driver.click(prev)
 
+
             day = driver.find_by_css("div.react-datepicker__month")
-            day = driver.find_by_css_with_obj(day, "div[aria-label=day-{day}]".format(day = now_date[2]))
+            day = driver.find_all_by_css_with_obj(day, "div[aria-label=day-{day}]".format(day = now_date[2]))
+            if now_date[2] > 15 and len(day) != 1:
+                day = day[1]
+            else:
+                day = day[0]
             driver.click(day)
 
     fitting = driver.find_by_css("div.resize-grid-btn > button:nth-child(2)")
     driver.click(fitting)
 
-
     result = []
     while True:
         container = driver.find_by_css("div.ag-body-container")
         if len(container.text.strip()) == 0:
+            labels = ['이름', '시급', '연장근무', '야간근무', '휴일근무', '근무인정시간', '총급여']
+            df = pd.DataFrame(result, columns=labels)
+            df.to_csv('data/albam/' + date + '.csv', index=False, mode='w', encoding='utf-8-sig')
             return driver
 
         rows = driver.find_all_by_css_with_obj(container, "div.ag-row-position-absolute")
         for i in rows:
-            username = driver.find_by_css_with_obj(i, "div[col-id=username]").text
-            fullWorkTime = driver.find_by_css_with_obj(i, "div[col-id=fullWorkTime]").text
-            totalTime = driver.find_by_css_with_obj(i, "div[col-id=totalTime]").text
-            start_time = driver.find_by_css_with_obj(i, "div[col-id=\'0\']")
-            start_time = driver.find_by_css_with_obj(start_time, "div.schedule-time-label div:nth-child(2)").text.strip("()")
-            end_time = driver.find_by_css_with_obj(i, "div[col-id=\'1\']")
-            end_time = driver.find_by_css_with_obj(end_time, "div.schedule-time-label div:nth-child(2)").text.strip("()")
+            username = driver.find_by_css_with_obj(i, "div[col-id=username]").text.strip()
+            total_time = driver.find_by_css_with_obj(i, "div[col-id=\'5\']").text.strip()
+            wage = driver.find_by_css_with_obj(i, "div[col-id=\'0\']").text.strip()
+            total_money = driver.find_by_css_with_obj(i, "div[col-id=\'12\']").text.strip()
 
 
-            result.append([username, start_time, end_time, fullWorkTime, totalTime])
+            result.append([username, wage, total_time, total_money])
         next_btn = driver.find_by_css("button[ref=btNext]")
         if next_btn.get_attribute("disabled"):
             break
         driver.click(next_btn)
 
 
-    labels = ['이름', '출근시간', '퇴근시간', '총근무시간', '근무인정시간']
+    labels = ['이름', '시급', '연장근무', '야간근무', '휴일근무', '근무인정시간', '총급여']
     df = pd.DataFrame(result, columns=labels)
     df.to_csv('data/albam/' + date + '.csv', index=False, mode='w', encoding='utf-8-sig')
 
